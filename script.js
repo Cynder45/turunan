016b21ad5c2056a103f9583eb029df4615c34ebe
Cynder45 <104248326+Cynder45@users.noreply.github.com>
2022-05-03 17:24:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+alert('Selamat Datang :D');
+var typed = new Typed(".autotyped", {
+    strings: [ "Terima Kasih", "Terima Gaji", "Gak Ada yang mau nanya ?", "Yakin?", "Oke deh"],
+    typeSpeed: 100,
+    backSpeed: 150,
+    loop: true,
+});
+
+
+menuBtn.onclick= function(){
+    if (  sideNav.style.right   == "-250px"){
+      sideNav.style.right = "0";
+      menu.src="images/close.png"
+    }
+    else{
+       sideNav.style.right = "-250px";
+       menu.src="images/menu.png"
+    }
+  }
+
+
+   function openImg(element){
+    window.open(element.src);
+}
+
---
+++ start.js
@@ -0,0 +1,10 @@
+var a = prompt ("Enter Username")
+if (a == "turunan")
+{
+    window.location.href="index.html";
+}
+else 
+{
+    alert("invalid details");
+    window.location.href = "start.html";
+}
